527ee2976f2012efcf347692cfd1bdc6336f19c3
Arthur Tainmont <arthurtainmont@Arthurs-Mac-mini.local>
2024-03-28 21:32:15 +0100

    NLP course 2 week 4 exerc

---
+++ natural-language-processing and LLM/NLP with Probabilistic Models/CBOW_activation_functions.py
@@ -0,0 +1,17 @@
+# In this lecture notebook you will be given an introduction to the continuous bag-of-words model, its activation functions and some considerations when working with Numpy.
+
+import numpy as np
+
+# Relu leaves positive numbers as they are and transform negative numbers into 0
+def relu(z):
+    result = z.copy()
+    result[result < 0] = 0
+    return result
+
+# Softmax returns a probability that lies between 0 and 1
+def softmax(z):
+    e_z = np.exp(z)
+    sum_e_z = np.sum(e_z)
+    return e_z / sum_e_z
+
+
